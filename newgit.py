3b14fc4bc134eb059818b0ef439b84430e432792
dseop <ehdtjq4650@naver.com>
2020-04-09 00:10:16 +0900

    4/8 마지막

--- newgit.py
+++ newgit.py
@@ -1,3 +1,5 @@
+import ..
+
 print('hello')
 
 ##일단 기본적인 것들을 코딩해보자
@@ -10,5 +12,10 @@ print(a+b)
 html = "www.yes24.co.kr"
 print(html)
 
-url = "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=07&PageNumber=2&FetchSize=80" 
+url = "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=07&FetchSize=80" 
 # ex) &year=2020&month=4&day=8 이것도 추가 가능
+pagenumber = "&PageNumber=%d" %1
+
+url_2 = url + pagenumber
+print(url_2)
+
