29a770ee261ce03d331d8d1c99fa04cb79a081ae
dseop <ehdtjq4650@naver.com>
2020-04-09 00:06:26 +0900

    4/8 마지막

--- newgit.py
+++ newgit.py
@@ -9,3 +9,6 @@ print(a+b)
 
 html = "www.yes24.co.kr"
 print(html)
+
+url = "http://www.yes24.com/24/category/bestseller?CategoryNumber=001001025&sumgb=07&PageNumber=2&FetchSize=80" 
+# ex) &year=2020&month=4&day=8 이것도 추가 가능
